eb81cc97be5e756bd7c5152e6c6f99dfdebe8f26
AbuHuraira537 <abouhuraira967@gmail.com>
2020-05-13 14:51:45 +0500

    Add project files.

---
+++ thetaonlinestore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace thetaonlinestore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
